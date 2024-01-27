752a00ed2da7dfe6a1c60681135c7bc2f5be4c4e
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-01-27 19:45:24 +0300

    init

---
+++ repack.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+epm tool eget https://raw.githubusercontent.com/alt-autorepacked/common/v0.2.0/common.sh
+. ./common.sh
+
+_package="discord"
+_download_url="https://discord.com/api/download?platform=linux&format=deb"
+
+_download() {
+    epm -y repack "$_download_url"
+}
+
+download_version=$(_get_version_from_download_url)
+remote_version=$(_check_version_from_remote)
+
+if [ "$remote_version" != "$download_version" ]; then
+    TAG="v$download_version"
+    _download
+    _add_repo_suffix
+    _create_release
+    echo "Release created: $TAG"
+else
+    echo "No new version to release. Current version: $download_version"
+fi
+
+rm common.sh
